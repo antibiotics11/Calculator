a7d0c69e3742de4187d6623ff64ee528b2c5936a
Antibiotics <abx@abx.pe.kr>
2022-04-02 03:50:52 +0900

    2의 보수 뺄셈 추가

--- classes/multiplication.calculation.class.php
+++ classes/multiplication.calculation.class.php
@@ -1,18 +1,14 @@
 <?php
-    namespace calculator\calculation;
-    if (!defined(__MAXBIT__)) define("__MAXBIT__", 8);    
+	namespace calculator\calculation;
+	if (!defined(__MAXBIT__)) define("__MAXBIT__", 8);    
 
-    class multiplication {
+	class multiplication {
+		
+		public static function multiply(array $num1, array $num2): array {
 
-	public static function multiply(array $num1, array $num2): array {
-/*
-		$tmp = array();
-		$result = array();
-
-		$num2_length = count($num2);
-
-		for ($i = 0; $i < $num2_length; $i++) {
- */
-        }
-
-    };
+			
+		
+		}
+		
+	};
+	
