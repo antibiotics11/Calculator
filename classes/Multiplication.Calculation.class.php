94db97f2418b1c79483f778ed9e1ee6c916e3c8d
antibiotics11 <abx@abx.pe.kr>
2022-04-09 16:24:28 +0900

    곱셈 구현

--- classes/Multiplication.Calculation.class.php
+++ classes/Multiplication.Calculation.class.php
@@ -4,9 +4,30 @@
 
 	class Multiplication {
 		
+		/** 곱셈 */
 		public static function multiply(array $num1, array $num2): array {
-
-			$result = array();
+			
+			$partial_product = array();
+			
+			// 피승수와 승수의 각 비트를 AND 연산하여 결과를 부분곱 배열에 입력
+			for ($i = 0; $i <= __MAXBIT__; $i++) {
+				for ($j = $i, $k = 0; $j <= __MAXBIT__; $j++, $k++) {
+					
+					$partial_product[$i][$j] = ($num2[$i] && $num1[$k]) ? 1 : 0; 
+					
+				}
+			}
+			
+			// result를 일단 0으로 설정
+			$result = \Calculator\Binary::dec_to_bin(0);
+			
+			for ($k = 0; $k <= __MAXBIT__; $k++) {
+				
+				// 각 부분곱의 부분합을 계산하여 result에 입력
+				$result = \Calculator\Calculation\Addition::add($partial_product[$k], $result);
+				
+			}
+			
 			return $result;
 		
 		}
