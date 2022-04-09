e4318294746375227100f5673c1e5f7d881939ce
antibiotics11 <abx@abx.pe.kr>
2022-04-09 18:07:53 +0900

    Undefined offset 에러 수정

--- classes/Multiplication.Calculation.class.php
+++ classes/Multiplication.Calculation.class.php
@@ -9,10 +9,13 @@
 			
 			$partial_product = array();
 			
-			// 피승수와 승수의 각 비트를 AND 연산하여 결과를 부분곱 배열에 입력
 			for ($i = 0; $i <= __MAXBIT__; $i++) {
+				
+				$partial_product[$i] = \Calculator\Binary::dec_to_bin(0);
+				
 				for ($j = $i, $k = 0; $j <= __MAXBIT__; $j++, $k++) {
 					
+					// 피승수와 승수의 각 비트를 AND 연산하여 결과를 부분곱 배열에 입력
 					$partial_product[$i][$j] = ($num2[$i] && $num1[$k]) ? 1 : 0; 
 					
 				}
