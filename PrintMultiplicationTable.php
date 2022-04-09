84d256e14f8e389a55e9950a077e21bb40ba7959
antibiotics11 <abx@abx.pe.kr>
2022-04-09 18:06:51 +0900

    구구단 출력 테스트

---
+++ PrintMultiplicationTable.php
@@ -0,0 +1,36 @@
+<?php
+
+	error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+	include_once "Calculation.class.php";
+
+	echo PHP_EOL."====================== Multiplication Table ======================".PHP_EOL;
+
+	for ($i = 1; $i <= 9; $i++) {
+		for($j = 1; $j <= 9; $j++) {
+
+			$expression = (string)($i."*".$j);
+
+			try {
+
+				$calc = new \Calculator\Calculation($expression);
+				$result = $calc->get_result();
+
+			} catch (Exception $e) {
+
+				echo $e.PHP_EOL;
+
+			}
+
+			echo $expression."=".$result.chr(9);
+
+			$calc = NULL;
+
+		}
+
+		echo PHP_EOL;
+
+	}
+
+	echo PHP_EOL."memory usage: ";
+	echo memory_get_usage(true)." bytes".PHP_EOL;
+	//echo round(memory_get_usage(true) / 1024, 2)." kb".PHP_EOL;
